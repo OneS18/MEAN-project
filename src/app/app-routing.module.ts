e04fb5db2f8263688134ae0996c235ef0baf85a0
OneS18 <diablo201888@gmail.com>
2022-03-11 00:14:16 +0200

    start frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: AppComponent,
+  },
+  {
+    path: "invoice-builder",
+    loadChildren:
+      "../app/invoice-builder/invoice-builder.module#InvoiceBuilderModule",
+  },
+  {
+    path: "**",
+    redirectTo: "invoice-builder",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
